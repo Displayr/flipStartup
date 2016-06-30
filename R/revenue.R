6bdb79173c9a847aa54fff43e42edcb3d5ec66d5
TimBock <tim.bock@numbers.net.au>
2016-06-30 17:38:29 +1000

    Adding "end" parameter to revenue and subscribers

--- R/revenue.R
+++ R/revenue.R
@@ -2,15 +2,18 @@
 #'
 #' @description Computes acquisition, by cohort.
 #' @param data A \code{data.frame} that has the same variables as a \code{RevenueData} object.
+#' @param end The time past which revenue is ignored (where its \code{from} data is greater than this value.
+#' Note that the default value, \code{Sys.time()} may not be in the same time zone as your other data, and this
+#' can cause unexpected results.
 #' @param by The time unit. E.g., "month".
 #' @details Computed based on being a subscribed on the last second of the time period.
 #' @return A vector showing number of subscribers over time.
 #'
 #' @importFrom lubridate '%within%' seconds
 #' @export
-Revenue <- function(data, by = "month")
+Revenue <- function(data, end = Sys.time(), by = "month")
 {
-    result <- Subscribers(data, by = "month", volume = TRUE)
+    result <- Subscribers(data, by = "month", end = end, volume = TRUE)
     class(result) <- c("Revenue", class(result))
     result
 }
--- R/subscribers.R
+++ R/subscribers.R
@@ -2,6 +2,9 @@
 #'
 #' @description Computes acquisition, by cohort.
 #' @param data A \code{data.frame} that has the same variables as a \code{RevenueData} object.
+#' @param end The time past which revenue is ignored (where its \code{from} data is greater than this value.
+#' Note that the default value, \code{Sys.time()} may not be in the same time zone as your other data, and this
+#' can cause unexpected results.
 #' @param by The time unit. E.g., "month".
 #' @param volume The number of subscribers in terms of their value.
 #' @details Computed based on being a subscribed on the last second of the time period.
@@ -9,24 +12,32 @@
 #'
 #' @importFrom lubridate '%within%' seconds floor_date
 #' @export
-Subscribers <- function(data, by = "month", volume = FALSE)
+Subscribers <- function(data, end = Sys.time(),  by = "month", volume = FALSE)
 {
     if (by != "month")
         stop("This function only works for 'by' of month.")
     if (!volume)
         data <- data[data$observation == 1, ]
     start <- min(data$from)
-    end <- min(max(data$from), floor_date(Sys.time(), by))
     n <- interval(start, end) %/% months(1) + 1
-    result <- rep(NA, n)
+    result <- rep(NA, n) + 1
     starts <- start + months(0:(n-1))
     names(result) <- starts
+    count <- 0
     for (i in 1:n)
     {
         result[i] <- if(volume)
             {
                 start <- starts[i]
-                sum(data$value[start >= data$from & start < data$to])
+                filt <- start >= data$from & start <= data$to
+                #print(c(sum(filt)))
+                #print(c("length",length(unique(data$id[filt]))))
+                # if (i == n){
+                #     print(start)
+                #     print(data[!filt, c("from", "to", "value", "id")])
+                #     print(sum(data[!filt, c("value")]))
+                # }
+                sum(data$value[start >= data$from & start <= data$to])
             }
             else
             {
