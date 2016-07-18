e210881299fe467602635d152cc76f2f6a9d4f8a
TimBock <tim.bock@numbers.net.au>
2016-07-18 11:43:00 +1000

    Layercake

--- R/charts.R
+++ R/charts.R
@@ -1,3 +1,27 @@
+#' \code{LayerCake}
+#'
+#' @description Plots revenue by year and start year, as a stacked revenue chart.
+#' @param data A \code{data.frame} that has the same variables as a \code{RevenueData} object.
+#' @return A plotly plot.
+#' @importFrom flipStandardCharts Chart
+#' @importFrom scales col_numeric
+#' @export
+LayerCake <- function(data)
+{
+    table <- Table(value ~ start.period + period, data = rd, FUN = sum)
+    names(dimnames(table)) <- c("Start", "Year")
+    table <- t(table)
+    #table <- table[nrow(table):1, ] # Reordering or legend
+    k <- nrow(table)
+
+    p <- Chart(table, type = "Stacked Area",
+          title = 'Revenue "layercake"',
+          colors = col_numeric("Blues", domain = NULL)(1:(k + 3))[-1:-3], legend.sort.order = "reversed")
+    layout(p, y.title = "Revenue")
+}
+
+
+
 #' \code{TimeSeriesColumnChart}
 #'
 #' @description Plots a time series as columns, optionally smoothing the data.
@@ -8,8 +32,8 @@
 #' @param smooth Smooth the data using \code{supmsu}.
 #' @param ... Parameters to be passed to plotly.
 #' @return A plotly plot.
-##' @importFrom plotly plot_ly
 #' @importFrom stats supsmu
+#' @importFrom plotly config plot_ly
 #' @export
 TimeSeriesColumnChart <- function(x, smooth = TRUE, title = "", ytitle = "",  series.name = "",  ...)
 {
@@ -20,6 +44,8 @@ TimeSeriesColumnChart <- function(x, smooth = TRUE, title = "", ytitle = "",  se
         y = x,
         name = series.name,
         type = "bar")
+    plotly::config(displayModeBar = FALSE)
+
     # Smoothing.
     if (smooth)
     {
@@ -32,10 +58,10 @@ TimeSeriesColumnChart <- function(x, smooth = TRUE, title = "", ytitle = "",  se
             dates <- paste0(period.names,"-15")
         y.fitted <- supsmu(as.Date(dates), x)$y
         p <- add_trace(p,
-                       x = period.names,
-                       y = y.fitted,
-                       name = "Fitted",
-                       type = "line")
+            x = period.names,
+            y = y.fitted,
+            name = "Fitted",
+            type = "line")
     }
     p <- layout(p,
                 xaxis = list(title = "",
@@ -44,3 +70,38 @@ TimeSeriesColumnChart <- function(x, smooth = TRUE, title = "", ytitle = "",  se
                 yaxis = list(title = ytitle), title = title)
     p
 }
+
+
+#' \code{Heatmap}
+#'
+#' @description A heatmap of a matrix.
+#' @param x A matrix.
+#' @param title The title of the chart.
+#' @return A plotly plot.
+#' @importFrom flipFormat FormatAsReal
+#' @importFrom plotly layout
+#' @export
+Heatmap <- function(x, title)
+{
+    if (missing(title))
+        title = deparse(substitute(x))
+    row.title <- names(dimnames(x))[1]
+    if (is.null(row.names))
+        row.names = ""
+    column.title <- names(dimnames(x))[2]
+    if (is.null(row.names))
+        row.names = ""
+    hover.text <- x#FormatAsReal(x, 2)
+    hover.text <- paste(title, hover.text)
+    hover.text <- matrix(hover.text, ncol = nrow(x))
+    p <- plot_ly(z = x,
+            x = rownames(x),
+            y = colnames(x),
+            text = hover.text,
+            hoverinfo = "text",
+            type = "heatmap",
+            colorbar = list(title = title, min = -1))
+    layout(p, xaxis = list(title = row.title),
+               yaxis = list(title = column.title))
+}
+
---
+++ tests/testthat/test-charts.R
@@ -0,0 +1,29 @@
+context("charts")
+
+
+# Heatmap.
+mydata <- mtcars[, c(1,3,4,5,6,7)]
+cormat <- round(cor(mydata),2)
+Heatmap(cormat)
+cormat[lower.tri(cormat)] <- NA
+Heatmap(cormat)
+names(dimnames(cormat)) <- c("dog","cat")
+Heatmap(cormat, title = "Churn")
+
+rd <- RevenueData(d$AUD, from , to, end = end, id = d$name, by = "year", subset = d$validInvoice == 1)
+retention <- Retention(rd)
+
+Heatmap(retention$index, "Retention")
+Heatmap(retention$volume * 100, "Retention (%)")
+Heatmap(retention$retention * 100, "Retention (%)")
+
+
+l <- LifetimeValue(rd)
+Heatmap(l$total, "Total")
+Heatmap(l$mean, "Mean")
+Heatmap(l$cumulative, "Cumulative")
+Heatmap(l$index, "Index")
+
+
+rd <- RevenueData(d$AUD, from , to, end = end, id = d$name, by = "year", subset = d$validInvoice == 1)
+LayerCake(rd)
