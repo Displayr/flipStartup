81b37daf7a836f7f2ff331900559f3beaa788e96
Matt Engdahl <matt.engdahl@gmail.com>
2016-10-13 09:30:49 +1100

    Chart amendments

--- R/growthaccounting.R
+++ R/growthaccounting.R
@@ -135,9 +135,11 @@ QuickRatioPlot <- function(x, supress.first = 1)
     }
     growth.table$Period <- rownames(growth.table)
     growth.table$Benchmark = 4
-    p <- plot_ly(x = rownames(growth.table), hoverinfo="x+text", text=sprintf("%.2f", growth.table$QuickRatio), y = growth.table$Quick.Ratio, type="line", name = "Quick Ratio<br>(New + Exp. + Res.) / (Con. + Churned)")#, marker = list(color = "green"))
-    p <- add_trace(p = p, x = rownames(growth.table), y = growth.table$Benchmark, type="scatter",
-                   hoverinfo="x+text", text=sprintf("%.2f", growth.table$Benchmark), name = "4 is excellent for a business SaaS")#, marker = list(color = "blue"))
+    p <- plot_ly(x = rownames(growth.table), hoverinfo="x+text", text=sprintf("%.2f", growth.table$Quick.Ratio), 
+                 y = growth.table$Quick.Ratio, type="scatter", mode = "lines", name = "Quick Ratio<br>(New + Exp. + Res.) / (Con. + Churned)")#, marker = list(color = "green"))
+    p <- add_trace(p = p, x = rownames(growth.table), y = growth.table$Benchmark, 
+                   hoverinfo="x+text", text=sprintf("%.2f", growth.table$Benchmark), 
+                   name = "4 is excellent for a business SaaS")#, marker = list(color = "blue"))
     p <- layout(p = p, xaxis = list(title = "Period"), yaxis = list(title = "Quick Ratio", range = c(0, max(growth.table$Quick.Ratio, 4))))
     p
 }
