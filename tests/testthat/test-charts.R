54d22024eec32bb8066695f0c622119c5c25b63f
TimBock <tim.bock@numbers.net.au>
2016-10-16 22:22:25 +1100

    Fixes for plotly upgrade

--- tests/testthat/test-charts.R
+++ tests/testthat/test-charts.R
@@ -1,5 +1,7 @@
 context("charts")
 library(flipStatistics)
+data(q.invoice.lines)
+d <- q.invoice.lines
 
 
 # Heatmap.
@@ -12,21 +14,31 @@ Heatmap(cormat)
 names(dimnames(cormat)) <- c("dog","cat")
 Heatmap(cormat, title = "Churn")
 
-rd <- RevenueData(d$AUD, from , to, end = end, id = d$name, by = "year", subset = d$validInvoice == 1)
-retention <- Retention(rd, FALSE)
-
-Heatmap(t(retention$index), "Retention")
-Heatmap(retention$volume * 100, "Retention (%)")
-Heatmap(retention$retention * 100, "Retention (%)")
 
 
-l <- LifetimeValue(rd)
-LifetimeValue(rd)
-Heatmap(l$total, "Total")
-Heatmap(l$mean, "Mean")
-Heatmap(l$cumulative, "Cumulative")
-Heatmap(l$index, "Index")
+data(q.invoice.lines)
+d <- q.invoice.lines
+by = "year"
+library(lubridate)
+today <- ISOdate(2016,6,30)
+tz(today) <- "GMT"
+today <- today + hours(12) - seconds(1)
+rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1)
+# retention <- Retention(rd, FALSE)
 
+# Heatmap(t(retention$index), "Retention")
+# Heatmap(retention$volume * 100, "Retention (%)")
+# Heatmap(retention$retention * 100, "Retention (%)")
+#
+#
+# l <- LifetimeValue(rd)
+# LifetimeValue(rd)
+# Heatmap(l$total, "Total")
+# Heatmap(l$mean, "Mean")
+# Heatmap(l$cumulative, "Cumulative")
+# Heatmap(l$index, "Index")
+#
+#
+# rd <- RevenueData(d$AUD, from , to, end = end, id = d$name, by = "year", subset = d$validInvoice == 1)
+# LayerCake(rd)
 
-rd <- RevenueData(d$AUD, from , to, end = end, id = d$name, by = "year", subset = d$validInvoice == 1)
-LayerCake(rd)
