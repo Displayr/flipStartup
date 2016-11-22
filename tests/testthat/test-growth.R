8b57a505d2c16467965694e794b473acadb033c1
TimBock <tim.bock@numbers.net.au>
2016-11-22 17:30:39 +1100

    Adding "by"

---
+++ tests/testthat/test-growth.R
@@ -0,0 +1,16 @@
+context("Revenue data")
+data(q.invoice.lines)
+d <- q.invoice.lines
+library(lubridate)
+Sys.setenv(TZ='GMT')
+end <-  ISOdate(2016,12,31)
+start <-  ISOdate(2012,7,1)
+by = "month"
+for (by in c("week", "month", "quarter", "year"))
+    test_that(paste("Creating RevenueData", by),
+          {
+            expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, start = start, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+            expect_error(capture.output(print(Growth(rd))), NA)
+})
+
+
--- tests/testthat/test-growthaccounting.R
+++ tests/testthat/test-growthaccounting.R
@@ -7,58 +7,6 @@ by = "year"
 library(lubridate)
 today <- ISOdate(2016,6,30)
 tz(today) <- "GMT"
-today <- today + hours(12) - seconds(1)
-for (by in c("month","quarter", "year"))
-test_that(by,
-{
-    ## Annual
-    # No profiling.
-    expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1)), NA)
-
-    # Adding profiling variables of the wrong dimension
-    expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = d)))
-    # Adding profiling variables of the correct dimension
-    unique.names <- sort(unique(d$name))
-    zprofiling <- d[match(unique.names, as.character(d$name)), ]
-    expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling)))
-    # Adding profiling variables with id shown in row names
-    rownames(zprofiling) <- unique.names
-    capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, id = d$name, end = ISOdate(2016,11,1), by = by, subset = d$validInvoice == 1, profiling = zprofiling))
-    # testing end
-    capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = today, id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling))
-
-
-
-    rg <- RevenueGrowthAccounting(rd)
-    plot(rg)
-    QuickRatioPlot(rg, 3)
-
-    w <- Waterfall(rg)
-    plot(w)
-    w <- Waterfall(rg, names(rg$Revenue)[length(names(rg$Revenue)) - 1])
-    plot(w)
-
-    ## Churn
-    TimeSeriesColumnChart(Churn(rd, volume = FALSE)$rates, tickformat = "%")
-    TimeSeriesColumnChart(Churn(rd, volume = TRUE)$rates, tickformat = "%")
-    TimeSeriesColumnChart(Churn(rd, volume = TRUE)$rates, tickformat = "%")
-    TimeSeriesColumnChart(Acquisition(rd, volume = FALSE)$counts)#, tickformat = "%")
-    TimeSeriesColumnChart(Acquisition(rd, volume = TRUE)$counts)#, tickformat = "%")
-
-    plot(Subscribers(rd, by = "month"))
-    plot(Revenue(rd))
-
-    #####################################
-    ####  Retention                  ####
-    #####################################
-
-    flipStatistics::Table(id ~ start.period, data = rd, FUN = function(x) length(unique(x)))
-    Retention(rd)
-    Growth(rd)
-    if (by == "annual")
-        LifetimeValue(rd)
-})
-
 
 
 
@@ -94,54 +42,107 @@ test_that("Financial year",
     calculated.from.filter <- sum(rdm$value[rdm$from >= twelveMonthsAgo & rdm$value <= yearEnd])
     expect_equal(as.numeric(annual.from.annual), calculated.from.filter)
 
-    Revenue(rdm[rdm$from >= twelveMonthsAgo & rdm$value <= yearEnd, ], end = today)
+    expect_error(Revenue(rdm[rdm$from >= twelveMonthsAgo & rdm$value <= yearEnd, ], end = today), NA)
 
 
     rv <- Revenue(rdm, end = today)
-    rv[length(rv)]
+    #rv[length(rv)]
+    #rv
 
-        annual.from.annual - rv[length(rv)]
-    Growth(rd, FALSE)
+    annual.from.annual - rv[length(rv)]
+    expect_error(g <- Growth(rd, FALSE), NA)
 
-    rv
-    Growth(rd, FALSE)
-
-
-    sum(rd$value[rd$from >= twelveMonthsAgo & rd$value <= yearEnd])
-    Table(value ~ from, data = rd, sum)
-
-    z <- Table(value ~ from, data = rdm, sum)
-    k <- length(z)
-    sum(z[(k-11):k])
-
-
-    table(rd$from)
-    rg <- RevenueGrowthAccounting(rd, remove.last = FALSE)
-    plot(rg)
-    QuickRatioPlot(rg, 3)
-
-
-    Growth(rd, FALSE)
-    w <- Waterfall(rg)
-    plot(w)
-    w <- Waterfall(rg, names(rg$Revenue)[length(names(rg$Revenue)) - 1])
-    plot(w)
-
-    ## Churn
-    plot(Acquisition(rd, volume = TRUE))
-
-    plot(Subscribers(rd))
-    plot(Revenue(rd))
-
-    #####################################
-    ####  Retention                  ####
-    #####################################
-
-    Table(id ~ start.period, data = rd, FUN = function(x) length(unique(x)))
-    Retention(rd)
-    Growth(rd)
-    suppressWarnings(LifetimeValue(rd))
+    # Growth(rd, FALSE)
+    #
+    #
+    # sum(rd$value[rd$from >= twelveMonthsAgo & rd$value <= yearEnd])
+    # Table(value ~ from, data = rd, sum)
+    #
+    # z <- Table(value ~ from, data = rdm, sum)
+    # k <- length(z)
+    # sum(z[(k-11):k])
+    #
+    #
+    # table(rd$from)
+    # rg <- RevenueGrowthAccounting(rd, remove.last = FALSE)
+    # plot(rg)
+    # QuickRatioPlot(rg, 3)
+    #
+    #
+    # Growth(rd, FALSE)
+    # w <- Waterfall(rg)
+    # plot(w)
+    # w <- Waterfall(rg, names(rg$Revenue)[length(names(rg$Revenue)) - 1])
+    # plot(w)
+    #
+    # ## Churn
+    # plot(Acquisition(rd, volume = TRUE))
+    #
+    # plot(Subscribers(rd))
+    # plot(Revenue(rd))
+    #
+    # #####################################
+    # ####  Retention                  ####
+    # #####################################
+    #
+    # Table(id ~ relationship.from.period, data = rd, FUN = function(x) length(unique(x)))
+    # Retention(rd)
+    # Growth(rd)
+    # suppressWarnings(LifetimeValue(rd))
 })
+
+
+# today <- today + hours(12) - seconds(1)
+# for (by in c("month","quarter", "year"))
+# test_that(by,
+# {
+#     ## Annual
+#     # No profiling.
+#     expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+#
+#     # Adding profiling variables of the wrong dimension
+#     expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = d)))
+#     # Adding profiling variables of the correct dimension
+#     unique.names <- sort(unique(d$name))
+#     zprofiling <- d[match(unique.names, as.character(d$name)), ]
+#     expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling)))
+#     # Adding profiling variables with id shown in row names
+#     rownames(zprofiling) <- unique.names
+#     capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, id = d$name, end = ISOdate(2016,11,1), by = by, subset = d$validInvoice == 1, profiling = zprofiling))
+#     # testing end
+#     capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = today, id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling))
+#
+#     rg <- RevenueGrowthAccounting(rd)
+#     plot(rg)
+#     QuickRatioPlot(rg, 3)
+#
+#     w <- Waterfall(rg)
+#     plot(w)
+#     w <- Waterfall(rg, names(rg$Revenue)[length(names(rg$Revenue)) - 1])
+#     plot(w)
+#
+#     ## Churn
+#     TimeSeriesColumnChart(Churn(rd, volume = FALSE)$rates, tickformat = "%")
+#     TimeSeriesColumnChart(Churn(rd, volume = TRUE)$rates, tickformat = "%")
+#     TimeSeriesColumnChart(Churn(rd, volume = TRUE)$rates, tickformat = "%")
+#     TimeSeriesColumnChart(Acquisition(rd, volume = FALSE)$counts)#, tickformat = "%")
+#     TimeSeriesColumnChart(Acquisition(rd, volume = TRUE)$counts)#, tickformat = "%")
+#
+#     plot(Subscribers(rd, by = "month"))
+#     plot(Revenue(rd))
+#
+#     #####################################
+#     ####  Retention                  ####
+#     #####################################
+#
+#     flipStatistics::Table(id ~ relationship.from.period, data = rd, FUN = function(x) length(unique(x)))
+#     Retention(rd)
+#     Growth(rd)
+#     if (by == "annual")
+#         LifetimeValue(rd)
+# })
+#
+#
 
 
 
