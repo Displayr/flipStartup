4e68f382fe5f8aec17a3c78b7d1d0b21860751c4
TimBock <tim.bock@numbers.net.au>
2016-11-22 17:30:25 +1100

    Adding "by"

--- R/retention.R
+++ R/retention.R
@@ -2,93 +2,135 @@
 #'
 #' @description Computes retention, by cohort.
 #' @param data A \code{data.frame} that has the same variables as a \code{RevenueData} object.
+#' @param subscription.length The length of the subscription: \code{"day"},
+#' \code{"week"}, \code{"month"}, \code{"quarter"}, or \code{"year"}.
 #' @param remove.last Remove the final period (as usually is incomplete).
 #' @details Where subscribers suspends their purchasing for a period, but purchases again later, the subscriber
-#' is asumed to have been retained during the period where the account was suspended.
+#' is asumed to have been retained during the period where the account was suspended. Where
+#' a subscriber for some reason had a subscription length that was different to the specified
+#' \code{"subscription.length"}, their revenue-weighted retention will be misattributed.
 #' @return A \code{\link{list}} containing the following elements:
-#'   \item{counts}{The number of subscribers per \code{period} by \code{start.period}}
+#'   \item{counts}{The number of subscribers per \code{period} by \code{subscriber.from.period}}
 #'   \item{index}{The percentage (proportion * 100) of subscribers to remain subscribers}
-#'   \item{retention}{Retention byper \code{period} by \code{start.period}}
-#'   \item{average}{Average subscriber retention.}
-#'   \item{churn}{subscriber churn (1 - retention).}
+#'   \item{retention}{Retention per \code{period} by \code{subscriber.from.period}}
+#'   \item{retention.volume}{Same as \code{retention}, except weighted by
+#'    the volumne (revenue) in the previous subscription renewal.}
+#'   \item{average.retention}{Average subscriber retention.}
+#'   \item{churn}{subscriber churn (1 - average.retention).}
 #'   \item{average.life.span}{Estimated average subscriber lifespan (1 / churn) subscriber retention.}
-#'   \item{average.volume}{Retention, weighted by subscriber value in the preceeding year.}
-#'   \item{churn.volume}{Churn, weighted by subscriber value in the preceeding year.}
-#'
+#'   \item{average.volume}{Retention, weighted by subscriber value in the preceeding period.}
+#'   \item{churn.volume}{Churn, weighted by subscriber value in the preceeding period.}
+#' @importFrom lubridate as_date
 #' @export
-Retention <- function(data, remove.last = TRUE)
+Retention <- function(data, subscription.length = "year", remove.last = TRUE)
 {
+    by <- attr(data, "by")
     data.id <- data[data$observation == 1, ]
-    #data.id$
-
-    counts <- xtabs(~ start.period + last.from.period, data = data.id)
-    counts <- counts[, ncol(counts):1]
-    counts <- t(apply(counts, 1, cumsum))
-    counts <- counts[,ncol(counts):1]
-    periods <- sort(unique(unlist(dimnames(counts))))
-    k <- length(periods)
-    counts.1 <- matrix(NA, k, k, dimnames = list(start.period = periods, period = periods))
-    counts.1[match(rownames(counts), periods), match(colnames(counts), periods)] <- counts
-    counts.1[Triangle(counts.1, "lower left")] <- NA
-    counts <- counts.1
+    data.id$final.end <- as_date(data.id$subscriber.to)
+    last.period <- as_date(max(PeriodNameToDate(data.id$subscriber.from.period, by)))
+    data.id$final.end[data.id$final.end > last.period] <- last.period
+    data.id$final.end <- Period(data.id$final.end, by)
+#    counts <- xtabs(~ subscriber.from.period + final.end, data = data.id)
+    #print("a7")
+    period.names <- unique(c(unique(data$from.period), unique(data$to.period)))
+    #print(period.names)
+    periods <- CompleteListPeriodNames(period.names, by)
+     #<- Period(dates, by)
+    #print("a8")
+  #  counts <- FillInMatrix(counts, dates, dates, 0)
+    #print("a9")
+ #   counts <- counts[, ncol(counts):1]
+    #print("b")
+   # counts <- t(apply(counts, 1, cumsum))
+#    counts <- counts[,ncol(counts):1]
+ #   periods <- sort(unique(unlist(dimnames(counts))))
+#print("dog")
+#print(periods)
+    n.periods <- length(periods)
+    #counts.1 <- matrix(NA, n.periods, n.periods, dimnames = list(start.period = periods, period = periods))
+    #counts.1[match(rownames(counts), periods), match(colnames(counts), periods)] <- counts
+    #print("c")
+   # counts.1[Triangle(counts.1, "lower left")] <- NA
+   # counts <- counts.1
     # Filling in periods where no churn occurred
-    for (r in 1:k)
-        for (c in (k - 1):min(k - 1, r))
-            if (is.na(counts[r,c]) & r <= c)
-                counts[r, c] <- counts[r, c + 1]
-    if (remove.last){
-        counts <- counts[-nrow(counts), -ncol(counts)]
-        periods <- periods[-k]
-        k <- k - 1
-
-    }
+#     for (r in 1:n.periods)
+#         for (c in (n.periods - 1):min(n.periods - 1, r))
+#             if (is.na(counts[r,c]) & r <= c)
+#                 counts[r, c] <- counts[r, c + 1]
+#     if (remove.last){
+#         counts <- counts[-nrow(counts), -ncol(counts)]
+#         periods <- periods[-n.periods]
+#         n.periods <- n.periods - 1
+#
+#     }
+# #print("d")
     # Volume-based retention
-
-    volume <- matrix(NA, k, k, dimnames = list(start.period = periods, period = periods))
+    #periods.per.subscription <- round(DaysPerPeriod(subscription.length) / DaysPerPeriod(by), 0)
+#print(periods.per.subscription)
+    retention.rate.volume <-
+        matrix(NA, n.periods, n.periods, dimnames = list(subscriber.from.period = periods, period = periods))
+    names(dimnames(retention.rate.volume)) <- c("Commenced", by)
+    n.subscriptions <- n.retained <- retention.rate <- retention.rate.volume
     total <- 0
     total.lost <- 0
-    for (r in 1:(k - 1))
-        for (c in (r + 1):k)
+    total.by.period <- rep(0, n.periods)
+    names(total.by.period) <- periods
+    loss.by.period <- total.by.period
+    # Computing the volumetric retention rate
+    for (r in 1:(n.periods))
+    {
+        start.period <- periods[r]
+        starters <- data$subscriber.from.period == start.period
+        for (c in r:(n.periods))
         {
-            start.period <- periods[r]
             period <- periods[c]
-            previous.period <- periods[c - 1]
-            base <- data$start.period == start.period & data$period == previous.period
-            #id.base <- unique(data$id[base])
-            retained <- data$start.period == start.period & data$period == period
-            id.retained <- unique(data$id[retained])
-            revenue.base <- sum(data$value[base], na.rm = TRUE)
-            revenue.lost <- sum(data$value[base & !(data$id %in% id.retained)], na.rm = TRUE)
-            volume[r, c] <- (revenue.base - revenue.lost) / revenue.base
+            base <- starters & data$from.period == period
+            revenue <- data$value[base]
+            churn <- data$churn[base]
+            ids <- data$id[base]
+            revenue.base <- sum(revenue, na.rm = TRUE)
+            revenue.lost <- sum(revenue[churn], na.rm = TRUE)
+            n.subscriptions[r, c] <- n.subscribers <- length(unique(ids))
+            #n.retained[r, c] <-  sum(base)
+            n.retained[r, c] <- retained <-  n.subscribers - length(unique(ids[churn]))
+            retention.rate[r, c] <- retained / n.subscribers
+#            print(c(r, c, n.subscribers, sum(churn)))
+            retention.rate.volume[r, c] <- (revenue.base - revenue.lost) / revenue.base
             total <- total + revenue.base
             total.lost <- total.lost + revenue.lost
+            loss.by.period[r] <- revenue.lost + loss.by.period[r]
+            total.by.period[r] <- revenue.base + total.by.period[r]
         }
-    # Computing overall statistics.
-    #k <- ncol(counts)
-    names(dimnames(counts)) <- c("Commenced", "Year")
-    retention <- counts[, -1] / counts[, -k]
-    average <- sum(retention * counts[, -k], na.rm = TRUE) / sum(counts[, -k], na.rm = TRUE)
-    churn <- 1 - average
-    average.volume = (total - total.lost) / total
-    churn.volume = 1 - average.volume
+    }
+    average.retention.rate <- sum(retention.rate * n.subscriptions, na.rm = TRUE) / sum(n.subscriptions, na.rm = TRUE)
+    retention.rate.by.period <- apply(retention.rate * n.subscriptions, 2, sum, na.rm = TRUE) / apply(n.subscriptions, 2, sum, na.rm = TRUE)
+    retention.rate.volume.by.period <- 1 - loss.by.period / total.by.period
+    churn <- 1 - average.retention.rate
+    average.retention.rate.volume = (total - total.lost) / total
+    churn.volume = 1 - average.retention.rate.volume
     average.lifespan <- 1 / churn
     average.lifespan.volume <- 1 / churn.volume
-    churn.first.period <- c(1 - diag(volume[,-1]), NA)
-    names(churn.first.period) <- rownames(volume)
+    churn.first.period <- diag(1 - retention.rate)
     mean.churn.first.period <- mean(churn.first.period, na.rm = TRUE)
-    estimated.volume.retention.by.year <- 1 - churn.volume * (churn.first.period / mean.churn.first.period)
-    estimated.volume.retention.by.year[length(estimated.volume.retention.by.year)] <- estimated.volume.retention.by.year[length(estimated.volume.retention.by.year) - 1]
-    list(counts = counts,
-         index = IndexDiagonal(counts),
-         volume = volume,
-         retention = retention,
-         estimated.volume.retention.by.year = estimated.volume.retention.by.year,
-         average = average,
+    estimated.volume.churn.by.period <- churn.volume * (churn.first.period / mean.churn.first.period)
+    list(n.subscriptions = n.subscriptions,
+         n.retained = n.retained,
+         retention.rate = retention.rate,
+         retention.rate.volume = retention.rate.volume,
+         #index = IndexDiagonal(n.subscriptions),
+         average.retention.rate = average.retention.rate,
+         average.retention.rate.volume = average.retention.rate.volume,
+         retention.rate.by.period = retention.rate.by.period,
+         retention.rate.volume.by.period = retention.rate.volume.by.period,
+         #estimated.volume.retention.by.period = estimated.volume.retention.by.period,
+         #average.retention = average.retention,
          churn = churn,
-         average.lifespan =average.lifespan,
-         average.lifespan.volume = average.lifespan.volume,
-         average.volume = average.volume,
-         churn.volume = churn.volume)
+         churn.volume = churn.volume,
+         churn.first.period = churn.first.period,
+         average.lifespan = average.lifespan,
+         average.lifespan.volume = average.lifespan.volume)
+         #average.volume = average.volume,
+    #    $ $)
 }
 
 
--- R/revenuedata.R
+++ R/revenuedata.R
@@ -1,12 +1,12 @@
 #' \code{RevenueData}
 #'
-#' @description Computes the  statistics for use in growth accounting of a startup.
+#' @description Cleans and tidies data for use in growth accounting computations for a startup.
 #' @param value A vector of containing the revenue per transaction.
 #' @param from A vector of class \code{POSIXct} or \code{POSIXt}, recording the date
 #' and time each subscription commences.
 #' @param to A vector of class \code{POSIXct} or \code{POSIXt}, recording the date
 #' and time each subscription ends
-#' @param begin The date at which the analysis outputs should commence. By default,
+#' @param start The date at which the analysis outputs should commence. By default,
 #' the earliest date recorded in \code{from}.
 #' @param end The date at which the analysis ends, which is used to determine churn.
 #' By default, the most recent  date recorded in \code{from}.
@@ -20,47 +20,40 @@
 #' the values of \code{id}.
 #' @param trim.id The maximum length of the strings to be used showing ID names (used to avoid situations where
 #' string names are so long as to make reading of tables impossible.
-#' @return A \code{\link{data.frame}} containing the following variables, along with any other variables in the \code{data}:
+#' @return A \code{\link{data.frame}} qhwew where the rows represent unique combinations of periods
+#' and subscribers. Where a subscriber has multiple transactions in a period, they are aggregated. Contains
+#' the  following variables, along with any other variables in the \code{data}:
 #'   \code{id}{The unique identifier.}
-#'   \code{value}{The value of the transaction.}
-#'   \code{from}{The commencement date of the subscription.}
-#'   \code{to}{The end-date of the subscription.}
-#'   \code{start} The date of first subscription.
+#'   \code{value}{The total fee/price for a subscription.}
+#'   \code{from}{The commencement date of a subscription.}
+#'   \code{from.period} The \code{period} of the subscription commencement as a character.
+#'   \code{period.counter} The number of the period, where 0 indicates the initial period.
+#'   \code{to}{The end-date of a subscription.}
+#'   \code{to.period} The \code{period} of the subscription end as a character.
+#'   \code{subscriber.from} The date of a customer's first subscription's commencement.
+#'   \code{subscriber.from.period} The \code{period} of \code{subscriber.from}.
+#'   \code{subscriber.to} The final date of their most recent subscription.
+#'   \code{subscriber.to.period} The period of \code{subscriber.to}.
 #'   \code{last.from} The \code{from} date of the most recent subscription.
-#'   \code{last.from.period} The \code{period} of the \code{last.from} date.
-#'   \code{end} The final date to be used in any analysis.
-#'   \code{start.period} The \code{period} of the beginning of the first subscription.
-#'   \code{last.period} The \code{period} of the end of the most recent subscription.
-#'   \code{churned} A \code{logical} indicating if the subscriber had ever ceased subscribing.
-#'   \code{churned} A \code{logical} indicating if the subscriber had ceased subscribing in that period.
-#'   \code{tenure.interval} A \code{interval} of \code{start} to \code{end}.
+#'   \code{last.from.period} The period of \code{last.from}.
+#'   \code{churned} A \code{logical} indicating if the subscriber had ceased subscribing prior to \code{end}.
+#'   \code{churn} A \code{logical} indicating if the subscriber had ceased subscribing in that period.
+#'   \code{tenure.interval} A \code{interval} of \code{subscriber.from} to \code{subscriber.to}.
 #'   \code{tenure} The number of whole periods from the begining of the first subscription
 #'   to the end of the most recent.
-#'   \code{period} The \code{period} of the subscription.
-#'   \code{period.counter} The number of the period, where 0 indicates the initial period.
-#'   \code{observation} The number of the subscription, starting from 1.
+#'   \code{observation} The number of the subscription for a particular customer, starting from 1.
 #'
 #' @importFrom lubridate year years quarter month week weeks day days interval floor_date
-#' @importFrom anytime anytime
 #' @export
-RevenueData <- function(value, from, to, begin = min(from), end = max(from), id, by = "year", subset = rep(TRUE, length(id)), profiling = NULL, trim.id = 50) #, tolerance = .01)
+RevenueData <- function(value, from, to, start = min(from), end = max(from), id, by = "year", subset = rep(TRUE, length(id)), profiling = NULL, trim.id = 50) #, tolerance = .01)
 {
-    begin <- anytime(begin)
-    end <- anytime(end)
+    Sys.setenv(TZ='GMT')
     # Units.
-     .period <- function(x)
-     {
-        if (by == "year")
-            return(format(floor_date(x, by),"%Y"))
-        if (by == "month" | by == "quarter")
-            return(format(floor_date(x, by),"%Y-%m"))
-        floor_date(data$from, by)
-     }
     units <- switch(by, days = days(1), week = weeks(1), month = months(1), quarter = months(3), year = years(1))
-    dys <- switch(by, year = 365.25, quarter = 365.25 / 4, month = 365.25 / 12, week = 7)
+    dys <- DaysPerPeriod(by)
     # Merging profiling data.
     end <- floor_date(end, by)
-    data <- data.frame(id = as.character(id), value, from = floor_date(from, by),  to = floor_date(to, by))
+    data <- data.frame(id = as.character(id), value, from = floor_date(from, by), to = floor_date(to, by))
     # Filtering data.
     n.initial <- nrow(data)
     cat(paste0(n.initial, " transactions.\n"))
@@ -71,13 +64,23 @@ RevenueData <- function(value, from, to, begin = min(from), end = max(from), id,
         data <- subset(data, subset = subset)
     }
     # Removing observations that start after the end.
-    n <- nrow(data)
-    n.start.too.late <- sum(data$from > end)
-    if (n.start.too.late > 0)
-    {
-        cat(paste0(n.start.too.late, " transactions removed as they start after the 'end' date.\n"))
-        data <- subset(data, subset = data$from <= end)
-    }
+    # n <- nrow(data)
+    # start.too.late <- data$from > end
+    # n.start.too.late <- sum(start.too.late)
+    # if (n.start.too.late > 0)
+    # {
+    #     cat(paste0(n.start.too.late, " transactions removed as the subscription starts after the 'end' date.\n"))
+    #     data <- subset(data, !start.too.late)
+    # }
+    # # Removing observations that end after start
+    # n <- nrow(data)
+    # end.too.early <- data$to < start
+    # n.end.too.early <- sum(end.too.early)
+    # if (n.end.too.early > 0)
+    # {
+    #     cat(paste0(n.end.too.early, " transactions removed as the subscription ends before the 'start' date.\n"))
+    #     data <- subset(data, subset = !end.too.early)
+    # }
     zero <- data$value == 0
     n.zero <- sum(zero)
     if (n.zero > 0)
@@ -95,13 +98,14 @@ RevenueData <- function(value, from, to, begin = min(from), end = max(from), id,
     n <- nrow(data)
     cat(paste0(n, " transactions remaining.\n"))
     # Aggregating transactions that occor in the same time period.
-    data <- aggregate(value ~ id + from + to, data = data, FUN = sum)
-    data$to.period <- .period(data$to)
+    data$to.period <- Period(data$to, by)
+    data <- aggregate(value ~ id + from + to, data = data, FUN = sum)#data <- aggregate(value ~ id + from + to, data = data, FUN = sum)
     n <- nrow(data)
-    cat(paste0(n, " aggregated transactions (i.e., with same beginning and end date) remaining.\n"))
+    cat(paste0(n, " aggregated transactions (i.e., summed together when sharing a from and end date) remaining.\n"))
     # Subscriber-level calculations.
     id.data <- aggregate(from ~ id, data, min)
-    names(id.data)[2] <- "start"
+    names(id.data)[2] <- "subscriber.from"
+    id.data$subscriber.to <- aggregate(to ~ id, data, max)[, 2]
     if (!is.null(profiling))
     {
         if (("id" %in% names(profiling)))
@@ -142,39 +146,44 @@ RevenueData <- function(value, from, to, begin = min(from), end = max(from), id,
     }
     # Creating time-based metrics.
     id.data$last.from <- aggregate(from ~ id, data, max)[, 1]
-    id.data$last.from.period <- .period(floor_date(aggregate(from ~ id, data, max)[, 2], by))
+    id.data$last.from.period <- Period(floor_date(aggregate(from ~ id, data, max)[, 2], by), by)
+#print("doga")
     cat(paste0(nrow(id.data), " subscribers.\n"))
-    id.data$end.from <- aggregate(from ~ id, data, max)$from
-    id.data$end.to <- aggregate(to ~ id, data, max)$to
-    periods.from.start <- interval(id.data$start, id.data$end.to) %/% units
-    id.data$start.period <- .period(id.data$start)
-    id.data$last.period <- .period(id.data$end.from)
-    id.data$churned <- id.data$end.to < end
+#id.data$end.from <- aggregate(from ~ id, data, max)$from
+#print("dogb")
+    id.data$subscription.to <- aggregate(to ~ id, data, max)$to
+#print("dob")
+    id.data$tenure.interval <- interval(id.data$subscriber.from, id.data$subscriber.to)
+    id.data$tenure <- id.data$tenure.interval %/% units
+#print("dogd")
+    id.data$subscriber.from.period <- Period(id.data$subscriber.from, by)
+    id.data$subscriber.to.period <- Period(id.data$subscriber.to, by)
+#id.data$last.period <- Period(id.data$end.from, by)
+#print("dog")
+    id.data$churned <- id.data$subscriber.to < end
+#print("cat")
     not.churned <- !id.data$churned
     if (sum(not.churned) == 0)
         stop("The analyses assume that 1 or more subscribers have churned. None are shown as having churned in the data.")
-    #renewal.date[not.churned] <- (renewal.date + units)[not.churned] # The renewal data of the current license period.
-    #print("b")
-    #id.data$renewal.date <- renewal.date#<- as.Date(ifelse(id.data$churned, renewal.date, ))
-    id.data$tenure.interval <- interval(id.data$start, id.data$end.to)
-    id.data$tenure <- id.data$tenure.interval %/% units
     # Merging.
     data <- merge(data, id.data, by = "id", all.x = TRUE, sort = TRUE)
-    data$period <- .period(data$from)
-    #data$end.period <- .period(data$from)
-    data$churn <- data$churned & data$period == data$last.period & .period(data$end.to) == .period(data$to)
-    #print(table(data$churn))
-    data$period.counter <- interval(data$start, data$from) %/% units
+    data$from.period <- Period(data$from, by)
+    data$to.period <- Period(data$to, by)
+    #data$period.date <- floor_date(data$from, by)
+    data$churn <- data$churned & data$from.period == data$last.from.period# & Period(data$relationship.to, by) == Period(data$to, by)
+    data$period.counter <- interval(data$subscriber.from, data$from) %/% units
     # Sorting.
     data <- data[order(data$id, data$from),]
     # Creating a variable indicating observation number. Randomly sorts ties.
-    observation <- rep(1, n)
+    observation <- rep(1, n <- nrow(data))
     ids <- data$id
-    for (i in 2:nrow(data))
+    for (i in 2:n)
         if (ids[i] == ids[i - 1])
             observation[i] = observation[i - 1] + 1
     data$observation <- observation
     data$id <- sub("\\s+$", "", as.character(data$id))
+    data <- data[data$from >= start & data$from <= end, ]
+    attr(data, "by") <- by
     class(data) <- c(class(data), "RevenueData")
-    data[anytime(data$period) >= begin & anytime(data$period) <= end, ]
+    data
 }
