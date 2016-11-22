56b3b4635518ef00c330b68ab0430d9ea38f6622
TimBock <tim.bock@numbers.net.au>
2016-11-22 17:30:33 +1100

    Adding "by"

--- tests/testthat/test-acquisition.R
+++ tests/testthat/test-acquisition.R
@@ -1,23 +1,48 @@
-context("Growth Accounting")
-
-library(flipStatistics)
+context("acquisition ")
 data(q.invoice.lines)
 d <- q.invoice.lines
-by = "year"
 library(lubridate)
-today <- ISOdate(2016,6,30)
-tz(today) <- "GMT"
-today <- today + hours(12) - seconds(1)
-for (by in c("month","quarter", "year"))
+Sys.setenv(TZ='GMT')
+end <-  ISOdate(2016,12,31)
+start <-  ISOdate(2012,7,1)
+for (by in c("week", "month", "quarter", "year"))
+    test_that(paste("Creating RevenueData", by),
+          {
+            expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, start = start, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+            expect_error(a <- Acquisition(rd, volume = FALSE), NA)
+            expect_error(p <- TimeSeriesColumnChart(a$counts, a$by, ytitle = "New customers"), NA)
+            expect_error(print(p), NA)
+            expect_error(a <- Acquisition(rd, volume = TRUE), NA)
+            expect_error(p <- TimeSeriesColumnChart(a$counts, a$by, ytitle = "New customers"), NA)
+            expect_error(capture.output(print(p)), NA)
+})
 
-by <- "quarter"
-#rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1)
-zprofiling <- d[match(unique.names, as.character(d$name)), ]
-rownames(zprofiling) <- unique.names
-    unique.names <- sort(unique(d$name))
-    zprofiling <- d[match(unique.names, as.character(d$name)), ]
-    names(zprofiling)[match("name", names(zprofiling))] <- "id"
-rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling)
 
-TimeSeriesColumnChart(Acquisition(rd, volume = FALSE)$counts)#, tickformat = "%")
-TimeSeriesColumnChart(Acquisition(rd, volume = TRUE)$counts)#, tickformat = "%")
+
+
+
+
+
+# context("Growth Accounting")
+#
+# library(flipStatistics)
+# data(q.invoice.lines)
+# d <- q.invoice.lines
+# by = "year"
+# library(lubridate)
+# today <- ISOdate(2016,6,30)
+# tz(today) <- "GMT"
+# today <- today + hours(12) - seconds(1)
+# for (by in c("month","quarter", "year"))
+#
+# by <- "quarter"
+# #rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1)
+# zprofiling <- d[match(unique.names, as.character(d$name)), ]
+# rownames(zprofiling) <- unique.names
+#     unique.names <- sort(unique(d$name))
+#     zprofiling <- d[match(unique.names, as.character(d$name)), ]
+#     names(zprofiling)[match("name", names(zprofiling))] <- "id"
+# rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = ISOdate(2016,06,14), id = d$name, by = by, subset = d$validInvoice == 1, profiling = zprofiling)
+#
+# TimeSeriesColumnChart(Acquisition(rd, volume = FALSE)$counts)#, tickformat = "%")
+# TimeSeriesColumnChart(Acquisition(rd, volume = TRUE)$counts)#, tickformat = "%")
---
+++ tests/testthat/test-churn.R
@@ -0,0 +1,35 @@
+context("Revenue data")
+data(q.invoice.lines)
+d <- q.invoice.lines
+library(lubridate)
+Sys.setenv(TZ='GMT')
+end <-  ISOdate(2016,6,30)
+#start <-  ISOdate(2012,7,1)
+by = "year"
+for (by in c("week", "month", "quarter", "year"))
+    test_that(paste("Creating RevenueData", by),
+          {
+            expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+            expect_error(ch <- Churn(rd, volume = FALSE), NA)
+            expect_error(p <- TimeSeriesColumnChart(ch$rate, ch$by, tickformat = "%"), NA)
+            expect_error(capture.output(print(p)), NA)
+})
+
+
+
+
+
+
+# s <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/school_earnings.csv")
+# s <- s[order(s$Men), ]
+# library(plotly)
+# p <- plot_ly(s, x = ~Women, y = ~School, name = "Women",
+#              mode = "markers", marker = list(color = "pink"), type = "scatter") %>%
+#   add_trace(x = ~Men, name = "Men", y = ~School, marker = list(color = "blue"),
+#             mode = "markers", type = "scatter") %>%
+#   layout(
+#     title = "Gender earnings disparity",
+#     xaxis = list(title = "Annual Salary (in thousands)"),
+#     margin = list(l = 65)
+#   )
+# p
