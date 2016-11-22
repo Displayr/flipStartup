c1e1ac9e3230dd53471805b4ef194beac87555cd
TimBock <tim.bock@numbers.net.au>
2016-11-22 17:31:15 +1100

    Adding "by"

---
+++ tests/testthat/test-retention.R
@@ -0,0 +1,22 @@
+context("retention")
+data(q.invoice.lines)
+d <- q.invoice.lines
+library(lubridate)
+Sys.setenv(TZ='GMT')
+end <-  ISOdate(2016,6,30)
+by = "week"
+for (by in c("week", "month", "quarter", "year"))
+    test_that(paste("Creating RevenueData", by),
+          {
+            expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+            expect_error(capture.output(r <- Retention(rd)), NA)
+            expect_error(expect_error(rint(r)), NA)
+})
+
+
+# rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, end = end, id = d$name, by = by, subset = d$validInvoice == 1)
+# r <- Retention(rd)
+# apply(r$retention, 2, mean, na.rm = TRUE)
+# r
+#
+# expect_error(print(r), NA)
