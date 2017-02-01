e84abba46667fecc451cfd1841d66e5955edd260
TimBock <tim.bock@numbers.net.au>
2017-02-01 18:00:17 +1100

    Fixing bug in LayerCake
    And adding unit test

---
+++ tests/testthat/test-layercake.R
@@ -0,0 +1,15 @@
+data(q.invoice.lines)
+d <- q.invoice.lines
+library(lubridate)
+Sys.setenv(TZ='GMT')
+end <-  ISOdate(2016,12,31)
+start <-  ISOdate(2012,7,1)
+by = "year"
+for (by in c("week", "month", "quarter", "year"))
+    test_that(paste("Layercake", by),
+          {
+            expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, start = start, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
+            expect_error((print(LayerCake(rd))), NA)
+})
+
+
