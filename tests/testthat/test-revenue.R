5034a082edf2520c53bbd051c8330c75278659d1
TimBock <tim.bock@numbers.net.au>
2017-04-26 08:58:59 +1000

    Updating test

--- tests/testthat/test-revenue.R
+++ tests/testthat/test-revenue.R
@@ -5,7 +5,7 @@ library(lubridate)
 Sys.setenv(TZ='GMT')
 end <-  ISOdate(2016,12,31)
 start <-  ISOdate(2012,7,1)
-by = "year"
+by = "week"
 for (by in c("week", "month", "quarter", "year"))
     test_that(paste("Creating RevenueData", by),
           {
