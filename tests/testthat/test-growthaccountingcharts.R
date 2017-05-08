c8019c3c745174b508154c298a346c0b5a08c0b3
TimBock <tim.bock@numbers.net.au>
2017-05-08 18:01:19 +1000

    Fixing a bug with RevenueGrowthAccounting(..., remove.last = TRUE)

--- tests/testthat/test-growthaccountingcharts.R
+++ tests/testthat/test-growthaccountingcharts.R
@@ -5,13 +5,15 @@ library(lubridate)
 Sys.setenv(TZ='GMT')
 end <-  ISOdate(2016,2,15)
 start <-  ISOdate(2012,7,1)
-
+by = "year"
 for (by in c("week", "month", "quarter", "year"))
     test_that(paste("Creating RevenueData", by),
           {
+              
             expect_error(capture.output(rd <- RevenueData(d$AUD, d$ValidFrom, d$ValidTo, start = start, end = end, id = d$name, by = by, subset = d$validInvoice == 1)), NA)
-            expect_error(rg <- RevenueGrowthAccounting(rd), NA)
-
+            expect_error(rg <- RevenueGrowthAccounting(rd, remove.last = FALSE), NA)
+            expect_error(print(plot(rg)), NA)
+            expect_error(rg <- RevenueGrowthAccounting(rd, remove.last = TRUE), NA)
             expect_error(print(plot(rg)), NA)
             expect_error(print(QuickRatioPlot(rg)), NA)
 
