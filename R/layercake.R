d1afb59fe14f91f07c72437e8db0b0c7ecc51c22
TimBock <tim.bock@numbers.net.au>
2017-11-14 09:15:12 +1100

    DS-1649 Fixing Layercake, by swapping *Chart* for *Area*

--- R/layercake.R
+++ R/layercake.R
@@ -4,7 +4,7 @@
 #' @param data A \code{data.frame} that has the same variables as a \code{RevenueData} object.
 #' @param title The title to show above the plot.
 #' @return A plotly plot.
-#' @importFrom flipStandardCharts Chart
+#' @importFrom flipStandardCharts Area
 #' @importFrom flipStatistics Table
 #' @importFrom scales col_numeric
 #' @export
@@ -13,12 +13,25 @@ LayerCake <- function(data, title = 'Revenue "layercake"')
     table <- Table(value ~ subscriber.from.period + from.period, data = data, FUN = sum)
     names(dimnames(table)) <- c("Start", "Year")
     k <- nrow(table)
-
-    p <- Chart(t(table), type = "Stacked Area",
+    date.format <- switch(attr(data, "subscription.length"),
+                          "year" = "%Y", "%b %Y")
+    p <- Area(t(table), type = "Stacked Area",
           title = title,
+          x.tick.format = date.format,
           colors = col_numeric("Blues", domain = NULL)(1:(k + 3))[-1:-3],
           legend.ascending = FALSE)
     layout(p$plotly.plot, yaxis = list(title = "Revenue"))
 }
-
-
+# 
+# 
+# X <- matrix(runif(9 * 5), 9, dimnames = list(from = 2008:2016, to = 2012:2016))
+# 
+#     flipStandardCharts::Chart(X, type = "Stacked Area",
+#           title = "My chart",
+#           colors = scales::col_numeric("Blues", domain = NULL)(1:(k + 3))[-1:-3],
+#           legend.ascending = FALSE)
+#     
+# flipStandardCharts::Area(X, type = "Stacked Area",
+#           title = "My chart",
+#           colors = scales::col_numeric("Blues", domain = NULL)(1:(k + 3))[-1:-3],
+#           legend.ascending = FALSE)
