06f4e2922bb6cc47f75dbe92119c5752f2405f20
Tim Bock <tim.bock@displayr.com>
2018-12-27 17:37:39 +1100

    Fixing Travis warning relating to waring in tests

--- R/revenue.R
+++ R/revenue.R
@@ -9,7 +9,6 @@
 #' @details Computed based on being a subscribed on the last second of the time period.
 #' @return A vector showing number of subscribers over time.
 #'
-#' @importFrom lubridate '%within%' seconds
 #' @export
 Revenue <- function(data, end = Sys.time(), by)
 {
